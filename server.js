a9c1df1b87b25db60a3e9ae6d1f138772d3ad4b9
Sheetal Jadhav <jsheetal695@gmail.com>
2026-04-22 11:55:04 +0530

    Correct Server file

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jyotirlingas-frontend.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
